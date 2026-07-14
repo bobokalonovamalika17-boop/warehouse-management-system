9ab628177ad5b3fbcfa86ab8a39ba3db8b6306f2
Malikakhon Bobokalonova <malikakhon.bobokalonova@alif.tj>
2026-07-14 18:03:45 +0500

    Addfunc add_product,delete_product

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
-from warehouse import show_products, show_menu
-from data import products
+# main.py
+# Главное меню
+# Отображается главное меню где пользователь может выбрать категории
 
+from warehouse import show_menu, show_products, add_product, delete_product
+
+from data import products
 
 
 def main():
     while True:
         show_menu()
         choice = input("Выберите действие: ").strip()
-        if choice == '1':
+        if choice == "1":
             show_products(items=products)
+        elif choice == '2':
+            add_product()
+        elif choice == '3':
+            delete_product()   
         else:
-            print('Нет такой опции')
-            break    
+            print("Нет такой опции")
+            break
 
 
 if __name__ == "__main__":
--- warehouse.py
+++ warehouse.py
@@ -1,18 +1,74 @@
+# warehouse.py
+# Функции которые потом вызываются в файле main.py
+
 import pandas as pd
+from data import products
+
 
 def show_menu():
+    """
+    Функция выводит главное меню для выбора пользователя.
+    """
     print()
-    print('==========================')
-    print('')
-
-
-
-
+    print("===========================")
+    print("WAREHOUSE MANAGEMENT SYSTEM ")
+    print("===========================")
+    print()
+    print("1. Показать товары")
+    print("2. Добавить товар")
+    print("3. Удалить товар")
+    print("4. Изменить товар")
+    print("5. Найти товар")
+    print("6. Продать товар")
+    print("7. Пополнить склад")
+    print("8. Показать категории")
+    print("9. Статистика ")
+    print("0. Выход")
+    print()
 
 
 def show_products(items: list):
     """
-    Это функция используется
+    Функция выводит товары на складе в виде таблицы.
     """
-    df = pd.DataFrame(items)
-    print(df)
+    if not items:
+        print("Склад пуст")
+    else:
+        df = pd.DataFrame(items)
+        print(df)
+
+
+def add_product():
+    if products == []:
+        new_id = 1
+    else:
+        new_id = products[-1]["id"] + 1
+    name = input("Название: ")
+    category = input("Категория: ")
+    price = input("Цена: ")
+    quantity = input("Количество: ")
+    thisdict = {
+        "id": new_id,
+        "name": name,
+        "category": category,
+        "price": price,
+        "quantity": quantity,
+    }
+    products.append(thisdict)
+    print("Товар добавлен")
+
+
+def delete_product():
+    id_to_delete = int(input("Введите ID товара: "))
+
+    for product in products:
+        if product["id"] == id_to_delete:
+            products.remove(product)
+            print("Товар удален.")
+            break
+    else:
+        print("Товар не найден.")
+
+
+def update_product():
+    
